7897a23994d87bd9b970f7a8439f9f5267a3f098
Hendro Lee <hendrolee39@gmail.com>
2023-09-29 17:10:10 +1000

    add: controllers functions

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas?type=fast
+  @Get()
+  getAllNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  // GET /ninjas/:id
+  @Get(':id')
+  getNinjaById(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /ninjas
+  @Post()
+  createNinja() {
+    return {};
+  }
+
+  // PUT /ninjas/:id
+  @Put(':id')
+  updateNinja(@Param('id') id: string) {
+    return { id };
+  }
+
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+}
