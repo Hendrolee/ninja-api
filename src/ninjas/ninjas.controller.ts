69cc104a07adb67c53fab236cd84452e458ab945
Hendro Lee <hendrolee39@gmail.com>
2023-10-25 22:43:32 +1100

    add: ninjas service functions

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -8,6 +9,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { NinjasService } from './ninjas.service';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -23,26 +26,24 @@ export class NinjasController {
   // GET /ninjas/:id
   @Get(':id')
   getNinjaById(@Param('id') id: string) {
-    return { id };
+    return this.ninjasService.getNinja(+id);
   }
 
   // POST /ninjas
   @Post()
-  createNinja() {
-    return {};
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return this.ninjasService.createNinja(createNinjaDto);
   }
 
   // PUT /ninjas/:id
   @Put(':id')
-  updateNinja(@Param('id') id: string) {
-    return { id };
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return this.ninjasService.updateNinja(+id, updateNinjaDto);
   }
 
   // DELETE /ninjas/:id
   @Delete(':id')
   removeNinja(@Param('id') id: string) {
-    return {
-      id,
-    };
+    return this.ninjasService.removeNinja(+id);
   }
 }
